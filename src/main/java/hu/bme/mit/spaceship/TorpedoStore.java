01be5dcef63d32cdbb4713934724b2d7cfb8c9ef
Botond János Kovács <botondjanoskovacs@gmail.com>
2020-05-04 23:04:41 +0200

    Random generator used for simulating overheating is now a member variable, and initialized via SecureRandom.getInstanceStrong()

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.spaceship;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -11,12 +13,16 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
+  private Random generator = null;
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-
+    try {
+      this.generator = SecureRandom.getInstanceStrong();
+    } catch(NoSuchAlgorithmException ex) {
+      throw new IllegalStateException("Cannot create torpedo store, random generator could not be initialized!", ex);
+    }
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
@@ -36,7 +42,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
