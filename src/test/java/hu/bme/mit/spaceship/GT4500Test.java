ddb23834ffacd5d946fef6d6582412c304981c78
Botond János Kovács <botondjanoskovacs@gmail.com>
2020-05-06 20:23:49 +0200

    Tests of the GT4500 spaceship now use mocked torpedo stores to verify firing functionality

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,56 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primaryTorpedoStore, 
+    secondaryTorpedoStore;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    this.primaryTorpedoStore = mock(TorpedoStore.class);
+    this.secondaryTorpedoStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(
+      this.primaryTorpedoStore,
+      this.secondaryTorpedoStore
+    );
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    // A single torpedo should have been fired from the primary store
+    verify(primaryTorpedoStore).fire(1);
+    // No torpedos should have been fired from the secondary store
+    verify(secondaryTorpedoStore, never()).fire(anyInt());
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.getTorpedoCount()).thenReturn(10);
+    when(primaryTorpedoStore.fire(anyInt())).thenReturn(true);
+
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.getTorpedoCount()).thenReturn(20);
+    when(secondaryTorpedoStore.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    // Reported 10 remaining torpedos, all of them should have been fired
+    verify(primaryTorpedoStore).fire(10);
+    // All 20 reported torpedos should have been fired from the secondary
+    verify(secondaryTorpedoStore).fire(20);
   }
 
 }
